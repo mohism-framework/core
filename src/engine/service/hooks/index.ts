3735fcea80b4b7c1039dcd32ed3ec4e3462a24d0
lanhao <admin@lanhao.name>
2021-03-18 16:57:19 +0800

    feat: 修复类型定义

--- src/engine/service/hooks/index.ts
+++ src/engine/service/hooks/index.ts
@@ -13,8 +13,8 @@ export const bindHooks = async (app: BaseApplication) => {
  * 这个方法可以获取到一个模型实例 🤡
  * @param name 数据模型的名字
  */
-export const useModel = <T>(name: string): Model<Document<T>> => {
-  return application?.models?.get(name) as Model<Document<T>>;
+export const useModel = <T extends Document>(name: string): Model<T> => {
+  return application?.models?.get(name) as unknown as Model<T>;
 };
 
 /**
